1dcd9b07c8830535ec90f3528675998017ed8d70
Chuckie Chang <546007561@qq.com>
2016-12-23 17:49:03 +0800

    Update

--- uix-slideshow.php
+++ uix-slideshow.php
@@ -116,12 +116,17 @@ class UixSlideshow {
 	 *
 	 */
 	public static function inc_str( $str, $incstr ) {
-	
-		if ( mb_strlen( strpos( $str, $incstr ), 'UTF8' ) > 0 ) {
-			return true;
+	    
+		if ( !empty( $incstr ) ) {
+			if ( preg_match( '/'.$incstr.'/', $str ) ) {
+				return true;
+			} else {
+				return false;
+			}
 		} else {
 			return false;
 		}
+
 
 	}
 
